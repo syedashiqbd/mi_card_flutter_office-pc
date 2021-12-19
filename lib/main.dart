b15ef58e12d18c89564af7507466edfdc629d030
Syed Ashiq <imsyedashiq@gmail.com>
2021-12-19 19:36:12 +0600

    adding Appbar and some text

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
+        appBar:AppBar(title: Center(child: Text('Bismillahir Rahmanir Rahim')),),
         body:SafeArea(
-          child: Container(child: Text('Hello'),
+          child: Center(
+            child: Container(
+              height: 150.0,
+              width: 200.0,
+              color: Colors.white,
+              child:Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+            ),
           ),
         ),
       ),
