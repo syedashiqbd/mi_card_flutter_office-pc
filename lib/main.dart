14677b8711587460810d4caf20e6049893564954
Syed Ashiq <imsyedashiq@gmail.com>
2021-12-21 11:44:58 +0600

    21-12-21/2

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        appBar:AppBar(title: Center(child: Text('Bismillahir Rahmanir Rahim')),),
-        body:SafeArea(
-          child: Center(
-            child: Container(
-              height: 250.0,
-              width: 300.0,
-              color: Colors.white,
-              child:Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
-            ),
-=======
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
-      title: Center(child: Text('Syed Ashiqur Rahman'),),),
+      title: Center(child: Text('Bismillahir Rahmanir Rahim'),),),
       body: SafeArea(
         child: Container(
           height: 200.0,
@@ -34,7 +20,6 @@ class MyApp extends StatelessWidget {
           padding: EdgeInsets.only(left: 15, top: 20),
           color: Colors.redAccent,
           child: Text("Hello. This is Syed Ashiq. I'm learning mobile application development. My chosing programe is Flutter framwork by DART"),
->>>>>>> 6948d374dae448729651768f3a4861c704c7dd9a
           ),
         ),
       )
