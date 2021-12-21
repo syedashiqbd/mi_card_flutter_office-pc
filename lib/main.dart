b01636e4096a71bd675b92ec88d2ec3e8e1be7c8
Syed Ashiq <imsyedashiq@gmail.com>
2021-12-21 11:26:54 +0600

    21-12-21

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ class MyApp extends StatelessWidget {
         body:SafeArea(
           child: Center(
             child: Container(
-              height: 150.0,
-              width: 200.0,
+              height: 250.0,
+              width: 300.0,
               color: Colors.white,
               child:Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
             ),
