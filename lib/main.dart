8999ae2dbc207c877938f8af101382a5ac90571c
Syed Ashiq <imsyedashiq@gmail.com>
2021-12-23 08:16:56 +0600

    email section added

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,48 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
+             Container(
+               color: Colors.white,
+               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+               padding: EdgeInsets.all(10.0),
+               child: Row(children:<Widget>[
+                 Icon(
+                   Icons.phone,
+                   color: Colors.teal,
+                   ),
+                   SizedBox(
+                     width: 15.0,
+                   ),
+                 Text(
+                   '+88 017 84 96 80 80',
+                   style: TextStyle(
+                     color: Colors.teal.shade900,
+                     fontSize: 20.0,
+                     fontFamily: 'Source Sans Pro',
+                   ),
+                 )
+               ],),
+             ),
+             Container(
+               color: Colors.white,
+               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+               padding: EdgeInsets.all(10.0),
+               child: Row(
+                 children:<Widget> [
+                   Icon(Icons.email, color: Colors.teal,),
+                   SizedBox(
+                     width: 15.0,
+                   ),
+                   Text('imsyedashiq@gmail.com',
+                   style: TextStyle(
+                     fontFamily: 'Source Sans Pro',
+                     fontSize: 20.0,
+                     color:Colors.teal.shade900,
+                   ),)
+                 ],
+               ),
+             )
           ],
-          //implement based on video No.46
           ),
         ),
       ),
