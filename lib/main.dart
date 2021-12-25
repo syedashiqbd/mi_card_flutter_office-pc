2f0da45a41975af83746988670514b6d7659c74e
Syed Ashiq <imsyedashiq@gmail.com>
2021-12-25 18:00:30 +0600

    Finished the visiting card making

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        
         body: SafeArea(
           child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
             children:<Widget> [
           CircleAvatar(
-            radius: 70.0,
+            radius: 70.0, 
             backgroundImage: AssetImage('images/ashiq.png'),
           ),
           Text(
@@ -39,47 +39,36 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-             Container(
-               color: Colors.white,
+             Card(
                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-               padding: EdgeInsets.all(10.0),
-               child: Row(children:<Widget>[
-                 Icon(
-                   Icons.phone,
-                   color: Colors.teal,
-                   ),
-                   SizedBox(
-                     width: 15.0,
-                   ),
-                 Text(
+               child:ListTile(
+                 leading: Icon(
+                   Icons.phone, 
+                   color: Colors.teal),
+                 title: Text(
                    '+88 017 84 96 80 80',
-                   style: TextStyle(
-                     color: Colors.teal.shade900,
-                     fontSize: 20.0,
-                     fontFamily: 'Source Sans Pro',
-                   ),
-                 )
-               ],),
-             ),
-             Container(
-               color: Colors.white,
-               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-               padding: EdgeInsets.all(10.0),
-               child: Row(
-                 children:<Widget> [
-                   Icon(Icons.email, color: Colors.teal,),
-                   SizedBox(
-                     width: 15.0,
-                   ),
-                   Text('imsyedashiq@gmail.com',
                    style: TextStyle(
                      fontFamily: 'Source Sans Pro',
                      fontSize: 20.0,
                      color:Colors.teal.shade900,
-                   ),)
-                 ],
+                   ),
+                   ),
                ),
-             )
+             ),
+             Card(
+               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+               child:ListTile(
+                 leading: Icon(Icons.email, color: Colors.teal),
+                 title: Text(
+                   'imsyedashiq@gmail.com',
+                   style: TextStyle(
+                     fontFamily: 'Source Sans Pro',
+                     fontSize: 20.0,
+                     color:Colors.teal.shade900,
+                  ),),
+                 ),
+               ),
+             
           ],
           ),
         ),
@@ -88,3 +77,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Row(
+//                  children:<Widget> [
+//                    Icon(Icons.email, color: Colors.teal,),
+//                    SizedBox(
+//                      width: 20.0,
+//                    ),
+//                    
+//                  ],
+//                ),
